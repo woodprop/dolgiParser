9ce860ce027e532e37156c03437281b0166e809c
AndreyG <gaponow@yandex.ru>
2020-03-24 17:54:38 +0300

    date input works

--- app.py
+++ app.py
@@ -5,15 +5,17 @@ from pyppeteer import launch
 from db import LinkDB
 
 base_url = 'http://bankrot.fedresurs.ru/'
-
+base_url_mes = 'https://bankrot.fedresurs.ru/Messages.aspx'
 
 def main():
+    html = asyncio.get_event_loop().run_until_complete(set_date_period(base_url_mes, 3000))
+
     db = LinkDB()
     # db.insert('hlkgsdkgjksd')
     # db.get_all()
     # db.create_web()
     # return
-    html = asyncio.get_event_loop().run_until_complete(get_html(base_url, 5000))
+    html = asyncio.get_event_loop().run_until_complete(get_html(base_url_mes, 2000))
     links = get_info(html)
     if not links:
         print('No links in response...')
@@ -21,14 +23,43 @@ def main():
 
     for link in links:
         print(link)
-        if check_link(link, 'номер'):
+        if check_link(link, 'квартира'):
             print('YES')
             db.insert(link)
 
     db.create_web()
 
 
+async def set_date_period(url, delay):
+    browser = await launch()
+    page = await browser.newPage()
 
+    await page.setViewport(viewport=dict(width=1920, height=4080))
+
+    await page.goto(url)
+    print('Loading page...')
+    await page.waitFor(delay)
+
+    await page.click('input[id="ctl00_cphBody_cldrBeginDate_tbSelectedDate"]')
+    await page.keyboard.down('Control')
+    await page.keyboard.press('KeyA')
+    await page.keyboard.up('Control')
+    await page.type('input[id="ctl00_cphBody_cldrBeginDate_tbSelectedDate"]', '04.02.2020')
+    await page.click('input[id="ctl00_cphBody_cldrEndDate_tbSelectedDate"]')
+    await page.keyboard.down('Control')
+    await page.keyboard.press('KeyA')
+    await page.keyboard.up('Control')
+    await page.type('input[id="ctl00_cphBody_cldrEndDate_tbSelectedDate"]', '05.02.2020')
+    await page.keyboard.press('Enter')
+
+    await page.waitFor(5000)
+    await page.screenshot({'path': 'example.png'})
+    html = await page.content()
+    soup = BeautifulSoup(html, 'html.parser')
+    inp = soup.find('input', id='ctl00_cphBody_cldrBeginDate_tbSelectedDate')
+
+    await browser.close()
+    return html
 
 
 async def get_html(url, delay):
@@ -47,7 +78,7 @@ def get_info(page):
     data = []
     soup = BeautifulSoup(page, 'html.parser')
 
-    links = soup.find_all('a', text=re.compile("Сообщение"))    # Ключевая фраза
+    links = soup.find_all('a', text=re.compile("Объявление о проведении торгов"))    # Ключевая фраза
 
     if links:
         for l in links:
@@ -63,8 +94,25 @@ def check_link(link, keyword):
     soup = BeautifulSoup(html, 'html.parser')
     lot_text = soup.find('div', class_='msg')
 
-    return keyword in lot_text.text
+ #   return keyword in lot_text.text
+    if 'здание' in lot_text.text:
+        print('здание')
+        return 1
 
+  #  if 'жилое' in lot_text.text:
+  #     print('жилое')
+  #      return 2
+
+    if 'помещение' in lot_text.text:
+        print('помещение')
+        return 3
+
+    if 'квартира' in lot_text.text:
+        print('квартира')
+        return 4
+    #
+    # if 'hjjlh' in lot_text.text:
+    #     return 32
 
 
 
--- db.py
+++ db.py
@@ -17,11 +17,11 @@ class LinkDB:
         print(self.cursor.fetchall())
 
     def create_web(self):
-        self.cursor.execute("SELECT DISTINCT link FROM links ORDER BY timestamp DESC")
+        self.cursor.execute("SELECT DISTINCT link, timestamp FROM links ORDER BY timestamp DESC")
         links = self.cursor.fetchall()
 
         file = open('links.html', 'w')
         for link in links:
-            tag = """<a href="{}" style="display: block">{}</a>""".format(link[0], link[0])
+            tag = """<a href="{}" style="display: block">{}</a>""".format(link[0], link[1])
             file.write(tag + '\n')
         file.close()

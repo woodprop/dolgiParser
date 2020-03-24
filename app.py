17840d47efd39dd0323ac86196d935d27217f4de
Konstantin <woodprop@yandex.ru>
2020-03-24 20:46:33 +0300

    done: input data in search-form, getting results

--- app.py
+++ app.py
@@ -8,14 +8,14 @@ base_url = 'http://bankrot.fedresurs.ru/'
 base_url_mes = 'https://bankrot.fedresurs.ru/Messages.aspx'
 
 def main():
-    html = asyncio.get_event_loop().run_until_complete(set_date_period(base_url_mes, 3000))
-
+    html = asyncio.get_event_loop().run_until_complete(get_search_result_page(base_url_mes, 3000))
+    # return
     db = LinkDB()
     # db.insert('hlkgsdkgjksd')
     # db.get_all()
     # db.create_web()
     # return
-    html = asyncio.get_event_loop().run_until_complete(get_html(base_url_mes, 2000))
+    # html = asyncio.get_event_loop().run_until_complete(get_html(base_url_mes, 2000))
     links = get_info(html)
     if not links:
         print('No links in response...')
@@ -27,41 +27,58 @@ def main():
             print('YES')
             db.insert(link)
 
+    # ---------- Сборка веб-страницы ----------
     db.create_web()
 
+# -------------------------------------------------------------------------------------------------------------------
 
-async def set_date_period(url, delay):
+async def get_search_result_page(url, delay):
     browser = await launch()
     page = await browser.newPage()
 
     await page.setViewport(viewport=dict(width=1920, height=4080))
 
     await page.goto(url)
-    print('Loading page...')
+    print('Загрузка страницы поиска...')
     await page.waitFor(delay)
 
+    # ---------- Клик на поле ТИП СООБЩЕНИЯ ----------
+    print('Выбор категирии и дат...')
+    await page.click('input[name="ctl00$cphBody$mdsMessageType$tbSelectedText"]')
+    await page.waitFor(2000)
+
+    # Раскрытие списка "Огранизация и проведение реализации имущества"
+    await page.mouse.click(200, 270)
+    await page.waitFor(1000)
+    # Клик на "Объявление о проведении торгов"
+    await page.mouse.click(200, 300)
+    await page.waitFor(1000)
+    # await page.screenshot({'path': 'example.png'})
+
+    # ---------- Ввод даты поиска ----------
     await page.click('input[id="ctl00_cphBody_cldrBeginDate_tbSelectedDate"]')
     await page.keyboard.down('Control')
     await page.keyboard.press('KeyA')
     await page.keyboard.up('Control')
-    await page.type('input[id="ctl00_cphBody_cldrBeginDate_tbSelectedDate"]', '04.02.2020')
+    await page.type('input[id="ctl00_cphBody_cldrBeginDate_tbSelectedDate"]', '01.03.2020')
+
     await page.click('input[id="ctl00_cphBody_cldrEndDate_tbSelectedDate"]')
     await page.keyboard.down('Control')
     await page.keyboard.press('KeyA')
     await page.keyboard.up('Control')
-    await page.type('input[id="ctl00_cphBody_cldrEndDate_tbSelectedDate"]', '05.02.2020')
+    await page.type('input[id="ctl00_cphBody_cldrEndDate_tbSelectedDate"]', '01.03.2020')
+
     await page.keyboard.press('Enter')
-
+    print('Поиск...')
     await page.waitFor(5000)
-    await page.screenshot({'path': 'example.png'})
+    # await page.screenshot({'path': 'example.png'})
     html = await page.content()
-    soup = BeautifulSoup(html, 'html.parser')
-    inp = soup.find('input', id='ctl00_cphBody_cldrBeginDate_tbSelectedDate')
-
     await browser.close()
+
     return html
 
 
+# >>>>>>>>>> Не используется, но пока храним <<<<<<<<<<
 async def get_html(url, delay):
     browser = await launch()
     page = await browser.newPage()
@@ -72,17 +89,16 @@ async def get_html(url, delay):
     html = await page.content()
     await browser.close()
     return html
+# ////////////////////////////////////////////////////////
 
 
 def get_info(page):
     data = []
     soup = BeautifulSoup(page, 'html.parser')
-
     links = soup.find_all('a', text=re.compile("Объявление о проведении торгов"))    # Ключевая фраза
 
     if links:
         for l in links:
-            # print(l)
             rawlink = l['onclick']
             link = base_url.replace('/', '') + rawlink.split('\'')[1]   # ппц, но пока пусть так
             data.append(link)
@@ -93,26 +109,19 @@ def check_link(link, keyword):
     html = asyncio.get_event_loop().run_until_complete(get_html(link, 2000))
     soup = BeautifulSoup(html, 'html.parser')
     lot_text = soup.find('div', class_='msg')
-
  #   return keyword in lot_text.text
+    # ToDo облагородить алгоритм поиска
     if 'здание' in lot_text.text:
         print('здание')
-        return 1
-
-  #  if 'жилое' in lot_text.text:
-  #     print('жилое')
-  #      return 2
+        return True
 
     if 'помещение' in lot_text.text:
         print('помещение')
-        return 3
+        return True
 
     if 'квартира' in lot_text.text:
         print('квартира')
-        return 4
-    #
-    # if 'hjjlh' in lot_text.text:
-    #     return 32
+        return True
 
 
 
